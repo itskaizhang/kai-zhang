07c8b87a5a31f9ceff6f77665f38f8e3b51da983
kyezhng <itskaizhang@gmail.com>
2025-12-20 09:15:37 -0500

    updates

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 // Source - https://stackoverflow.com/a
 // Posted by Anatoliy, modified by community. See post 'Timeline' for change history
 // Retrieved 2025-12-18, License - CC BY-SA 3.0
-
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -13,7 +12,6 @@ function getRandomColor() {
     return color;
   }
   
-
 document.querySelectorAll('#images').forEach(img => {
     img.addEventListener('mouseenter', () => {
         document.body.style.backgroundColor = getRandomColor();
@@ -22,4 +20,66 @@ document.querySelectorAll('#images').forEach(img => {
     img.addEventListener('mouseleave', () => {
     // document.body.style.backgroundColor = "";
     });
+});
+
+
+// Vertical highlight for zines, work, contact in index.html
+document.querySelectorAll('.zine-link').forEach(a => {
+    a.addEventListener('mouseenter', () => {
+        document.querySelectorAll('.zine-link').forEach(b => {
+            b.style.backgroundColor = '#056f04';
+            b.style.color = '#FAF7F0';
+        })
+    });
+    
+    a.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.zine-link').forEach(b => {
+            b.style.backgroundColor = '';
+            b.style.color = '';
+        })
+    });
+});
+
+document.querySelectorAll('.work-link').forEach(a => {
+    a.addEventListener('mouseenter', () => {
+        document.querySelectorAll('.work-link').forEach(b => {
+            b.style.backgroundColor = '#056f04';
+            b.style.color = '#FAF7F0';
+        })
+    });
+    
+    a.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.work-link').forEach(b => {
+            b.style.backgroundColor = '';
+            b.style.color = '';
+        })
+    });
+});
+
+document.querySelectorAll('.contact-link').forEach(a => {
+    a.addEventListener('mouseenter', () => {
+        document.querySelectorAll('.contact-link').forEach(b => {
+            b.style.backgroundColor = '#056f04';
+            b.style.color = '#FAF7F0';
+        })
+    });
+    
+    a.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.contact-link').forEach(b => {
+            b.style.backgroundColor = '';
+            b.style.color = '';
+        })
+    });
+});
+
+
+// Kai face change
+document.getElementById('kai-1').addEventListener('mouseenter', () => {
+    document.getElementById('kai-1').style.display = 'none';
+    document.getElementById('kai-2').style.display = 'inline';
+});
+
+document.getElementById('kai-2').addEventListener('mouseleave', () => {
+    document.getElementById('kai-1').style.display = 'inline';
+    document.getElementById('kai-2').style.display = 'none';
 });
---
+++ work/work.js
@@ -0,0 +1,40 @@
+// work.html pop-up windows
+document.getElementById('itsy-bitsy').addEventListener('click', () => {
+    const winWidth = 500;
+    const winHeight = 750;
+    const left = Math.round((screen.width - winWidth) / 2);
+    const top = Math.round((screen.height - (winHeight + 100) ) / 2);
+    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+                width=${winWidth},height=${winHeight},left=${left},top=${top}`;
+    let newWin = window.open("/work/itsy-bitsy.html", "itsy-bitsy", params);
+    newWin.document.close();
+});
+
+document.getElementById('itsy-bitsy').addEventListener('mouseenter', () => {
+    
+});
+
+
+
+document.getElementById('weidao-house').addEventListener('click', () => {
+    const winWidth = 1200;
+    const winHeight = 700;
+    const left = Math.round((screen.width - winWidth) / 2);
+    const top = Math.round((screen.height - (winHeight + 100) ) / 2);
+    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+                width=${winWidth},height=${winHeight},left=${left},top=${top}`;
+    let newWin = window.open("http://weidao.house/", "itsy-bitsy", params);
+    newWin.document.close();
+});
+
+document.getElementById('hao-qiu').addEventListener('click', () => {
+    const winWidth = 1200;
+    const winHeight = 700;
+    const left = Math.round((screen.width - winWidth) / 2);
+    const top = Math.round((screen.height - (winHeight + 100) ) / 2);
+    let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+                width=${winWidth},height=${winHeight},left=${left},top=${top}`;
+    let newWin = window.open("/work/hao-qiu.html", "itsy-bitsy", params);
+    newWin.document.close();
+});
+
