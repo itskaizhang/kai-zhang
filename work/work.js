1c934bf473a245425371e2a61445ed99f8e7d596
kyezhng <itskaizhang@gmail.com>
2025-12-20 23:03:00 -0500

    commit

--- work/work.js
+++ work/work.js
@@ -6,7 +6,7 @@ document.getElementById('itsy-bitsy').addEventListener('click', () => {
     const top = Math.round((screen.height - (winHeight + 100) ) / 2);
     let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
                 width=${winWidth},height=${winHeight},left=${left},top=${top}`;
-    let newWin = window.open("work/itsy-bitsy.html", "itsy-bitsy", params);
+    let newWin = window.open("work/itsy-preview.html", "itsy-bitsy", params);
     newWin.document.close();
 });
 
